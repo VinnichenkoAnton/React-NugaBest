0249d8e223f9344a944ded384a67db3802f850db
Vinnichenko Anton <antonnugabest@gmail.com>
2022-12-08 20:10:02 +0200

    MainScreen Component was changed: some layouts and styling for img was added, the build was performed

--- src/pages/MainPage/sections/MainScreen/MainScreen.tsx
+++ src/pages/MainPage/sections/MainScreen/MainScreen.tsx
@@ -2,12 +2,16 @@ import Container from '../../../../components/UI/Container/Container';
 import classes from './MainScreen.module.scss';
 
 import backgroundImg from '../../../../resources/mainScreen/mainScreen.jpg';
+import backgroundImg2 from '../../../../resources/mainScreen/mainScreen2.jpg';
 
 const MainScreen: React.FC = () => {
   return (
     <section className={classes.mainscreen}>
       <div className={classes.mainscreen__background}>
-        <img src={backgroundImg} alt="N5" />
+        <picture>
+          <source media="(max-width: 576px)" srcSet={backgroundImg2}></source>
+          <img src={backgroundImg} alt="N5" />
+        </picture>
       </div>
       <Container>
         <h2 className={classes.mainscreen__title}>BOL U LEĐIMA?</h2>
